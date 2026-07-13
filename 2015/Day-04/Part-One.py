624ebd1e2d46d0388e8681e4355567347400c917
toavinaAndria21 <rabezandrinytoavina@gmail.com>
2026-07-13 15:18:47 +0300

    refactor: 2015 Part 1

--- 2015/Day-04/Part-One.py
+++ 2015/Day-04/Part-One.py
@@ -1,8 +1,9 @@
 import hashlib
 
 i = 0
+input = 'yzbqklnj'
 while True :
-    secret_key = 'yzbqklnj' + str(i)
+    secret_key = input + str(i)
     md5_hash = hashlib.md5(secret_key.encode())
     string_hash = str(md5_hash.hexdigest())
 
